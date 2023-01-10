38df5a48db8dc72489fd934c64dffafea1ae4703
WAM <eraserw@gmail.com>
2023-01-10 14:08:29 +0100

    feat: filtres

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.get("/tickets", async (req, res) => {
     try {
         const data = await client.query('select * from tickets order by id');
 
-        data.rowCount >= 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" });
-        console.log(tabl_Erreurs[200]);
+        data.rowCount >= 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" ,message: "tableau vide !!"});
+
     }
     catch (err) {
         res.redirect()
@@ -53,17 +53,25 @@ app.get("/tickets/:id", async (req, res) => {
     try {
 
         const id = req.params.id;
+        console.log(id);
+        const dataId = await client.query('select id from tickets order by id');
+        const realId = dataId.rows.map(data => data.id);
 
         const sql = 'select * from tickets where id = $1';
-
         const data = await client.query(sql, [id]);
 
-        data.rowCount === 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" });
+        if(realId.includes(parseInt(id))){
+
+            data.rowCount === 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" }) }
+
+        else {
+            res.json({ status: "NOT FOUND", conseil: "vérifiez votre id dans la liste suivante", list: realId }) ;
+        }
 
 
     }
     catch (err) {
-        
+
         res.status(404).json({ status: "NOT FOUND" });
         res.status(400).json({ status: "BAD REQUEST" });
 
@@ -73,11 +81,11 @@ app.post("/tickets", async (req, res) => {
     try {
         const newRequest = req.body.message;
 
-        const sql = 'insert into tickets (message) values ($1)';
+        const sql = 'insert into tickets (message) values ($1) returning *';
 
         const data = await client.query(sql, [newRequest]);
 
-        res.status(201).json({ status: "CREATED", data: req.body })
+        res.status(201).json({ status: "CREATED", data: data.rows })
 
     } catch (error) {
         res.status(404).json({ status: "BAD REQUEST" });
@@ -108,10 +116,11 @@ app.delete("/tickets/:id", async (req, res) => {
         const id = req.params.id;
 
         const sql = 'delete from tickets where id = $1';
+        const dataId = await client.query('select * from tickets where id = $1',[id]);
 
         const data = await client.query(sql, [id]);
 
-        data.rowCount === 1 ? res.status(200).json({ status: "NO CONTENT", deleted: true }) : res.status(400).json({ status: "NO CONTENT", deleted: false, conseil: "vérifiez votre numéro de ticket" });
+        data.rowCount === 1 ? res.status(200).json({ status: "NO CONTENT", deleted: true ,lastSave: dataId.rows}) : res.status(400).json({ status: "NO CONTENT", deleted: false, conseil: "vérifiez votre numéro de ticket" });
 
     } catch (error) {
         res.status(404).json({ status: "BAD REQUEST" });
