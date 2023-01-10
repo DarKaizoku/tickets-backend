c1fa0bbe43dbc3a118268291d1d34c32c4685740
WAM <eraserw@gmail.com>
2023-01-10 15:04:21 +0100

    fix structure status

--- index.js
+++ index.js
@@ -37,11 +37,13 @@ app.use(function (req, res, next) {
 
 });
 
-app.get("/tickets", async (req, res) => {
+app.get("/API/tickets", async (req, res) => {
     try {
         const data = await client.query('select * from tickets order by id');
 
-        data.rowCount >= 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" ,message: "tableau vide !!"});
+        const result = {post : data.rows};
+
+        data.rowCount >= 1 ? res.status(200).json({ status: "SUCCESS", data: result }) : res.status(200).json({ status: "NOT FOUND" ,message: "tableau vide !!"});
 
     }
     catch (err) {
@@ -49,20 +51,21 @@ app.get("/tickets", async (req, res) => {
         res.status(400).json({ status: "BAD REQUEST" });
     }
 });
-app.get("/tickets/:id", async (req, res) => {
+app.get("/API/tickets/:id", async (req, res) => {
     try {
 
         const id = req.params.id;
-        console.log(id);
+        
         const dataId = await client.query('select id from tickets order by id');
         const realId = dataId.rows.map(data => data.id);
 
         const sql = 'select * from tickets where id = $1';
         const data = await client.query(sql, [id]);
+        const result = {post : data.rows};
 
         if(realId.includes(parseInt(id))){
 
-            data.rowCount === 1 ? res.status(200).json({ status: "SUCCESS", data: data.rows }) : res.status(200).json({ status: "NOT FOUND" }) }
+            data.rowCount === 1 ? res.status(200).json({ status: "SUCCESS", data: result }) : res.status(200).json({ status: "NOT FOUND" }) }
 
         else {
             res.json({ status: "NOT FOUND", conseil: "vérifiez votre id dans la liste suivante", list: realId }) ;
@@ -77,22 +80,24 @@ app.get("/tickets/:id", async (req, res) => {
 
     }
 })
-app.post("/tickets", async (req, res) => {
+app.post("/API/tickets", async (req, res) => {
     try {
-        const newRequest = req.body.message;
+        const message = req.body.message;
+        if(message === ("" || null || undefined)){res.status(406).json({ status: "FAIL" ,data : {message: "merci de saisir un message pour valider la requete !"}});}
 
         const sql = 'insert into tickets (message) values ($1) returning *';
 
-        const data = await client.query(sql, [newRequest]);
+        const data = await client.query(sql, [message]);
+        const result = {post : data.rows};
 
-        res.status(201).json({ status: "CREATED", data: data.rows })
+        res.status(201).json({ status: "CREATED", data: result })
 
     } catch (error) {
         res.status(404).json({ status: "BAD REQUEST" });
         res.status(400).json({ status: "BAD REQUEST" });
     }
 })
-app.put("/tickets/:id", async (req, res) => {
+app.put("/API/tickets/:id", async (req, res) => {
     try {
 
         const id = req.params.id;
@@ -110,7 +115,7 @@ app.put("/tickets/:id", async (req, res) => {
         res.status(405).json({ status: "METHOD NOT ALLOWED" });
     }
 })
-app.delete("/tickets/:id", async (req, res) => {
+app.delete("/API/tickets/:id", async (req, res) => {
     try {
 
         const id = req.params.id;
@@ -120,7 +125,7 @@ app.delete("/tickets/:id", async (req, res) => {
 
         const data = await client.query(sql, [id]);
 
-        data.rowCount === 1 ? res.status(200).json({ status: "NO CONTENT", deleted: true ,lastSave: dataId.rows}) : res.status(400).json({ status: "NO CONTENT", deleted: false, conseil: "vérifiez votre numéro de ticket" });
+        data.rowCount === 1 ? res.status(200).json({ status: "SUCCESS",data : null, deleted: true ,lastSave: dataId.rows}) : res.status(400).json({ status: "NO CONTENT", deleted: false, conseil: "vérifiez votre numéro de ticket" });
 
     } catch (error) {
         res.status(404).json({ status: "BAD REQUEST" });
